6f05deb50b8e8c84a78d5b12bd6a9639e575e755
Andrew Battye <andrew.battye@sap.com>
2016-05-10 19:23:02 +0100

    Improve ASR driver as per manila - to use configured physical network

--- networking_asr/plugins/ml2/drivers/mech_asr/driver.py
+++ networking_asr/plugins/ml2/drivers/mech_asr/driver.py
@@ -22,38 +22,62 @@ from neutron.i18n import _LI
 from neutron.i18n import _LW
 from neutron.plugins.ml2 import driver_api as api
 
+from neutron.plugins.common import constants as p_constants
+
+
+import constants
+
+
+
 LOG = logging.getLogger(__name__)
 
 
-class CiscoASRMechanismDriver(api.MechanismDriver):
+cfg.CONF.import_group('ml2_asr',
+                      'networking_asr.plugins.ml2.drivers.mech_asr.config')
 
+
+class CiscoASRMechanismDriver(api.MechanismDriver):
     def __init__(self):
         LOG.info(_LI("ASR mechanism driver initializing..."))
 
-        self.vif_type = "asr"
+        self.vif_type = constants.VIF_TYPE_ASR
         self.vif_details = {portbindings.CAP_PORT_FILTER: False}
+        self.physical_networks = cfg.CONF.ml2_asr.physical_networks
 
+        super(CiscoASRMechanismDriver, self).__init__()
 
-
+        LOG.info(_LI("ASR mechanism driver initialized."))
 
     def initialize(self):
         pass
 
-
     def bind_port(self, context):
-
-        network_id = context.network.current['id']
-
-        # For now we will just bind the last segment
-        if context.segments_to_bind:
-            segment = context.segments_to_bind.pop()
-
-            device_owner = context.current['device_owner']
-
-            # For now use the simple check that its a network object,
-            # needs refinement because this will include DHCP agents
-            # Workaround now by putting asr driver at end of list in config
-            if device_owner and device_owner.startswith('network'):
-                context.set_binding(segment[api.ID], self.vif_type,self.vif_details)
+        device_owner = context.current['device_owner']
+        if device_owner and device_owner.startswith('network:router'):
+            # bind to first segment if no physical networks are configured
+            if self.physical_networks is None:
+                self._set_binding(context, context.segments_to_bind[0])
                 return True
+
+
+            for segment in context.segments_to_bind:
+                if segment[api.PHYSICAL_NETWORK] in self.physical_networks:
+                    self._set_binding(context, segment)
+
+                    LOG.debug("Set binding for port %(port)s for physical network %(physical_network)s", {'port':str(context.current[api.ID]), 'physical_network': str(segment[api.PHYSICAL_NETWORK])})
+
+                    return True
+
+            LOG.error("No segment matches the configured physical networks "
+                      "%(physical_networks)s",
+                      {'physical_networks': self.physical_networks})
+        return False
+
+    def _set_binding(self, context, segment):
+        context.set_binding(segment[api.ID],
+                            self.vif_type,
+                            self.vif_details,
+                            p_constants.ACTIVE)
+
+
 
